8fd976fa0987a8c7c3692b831b0dfee9ca133621
Cristian Merli <cristian@thirdstream.ca>
2017-05-03 11:57:03 -0600

    Api demo setup

---
+++ src/AppBundle/Controller/PersonController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Person;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Person controller.
+ *
+ * @Route("persons")
+ */
+class PersonController extends Controller
+{
+    /**
+     * Lists all person entities.
+     *
+     * @Route("/", name="person_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        //***** Here i want to use GET /api/persons/
+
+        $people = $em->getRepository('AppBundle:Person')->findAll();
+
+        return $this->render('person/index.html.twig', array(
+            'people' => $people,
+        ));
+    }
+
+    /**
+     * Creates a new person entity.
+     *
+     * @Route("/new", name="person_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $person = new Person();
+        $form = $this->createForm('AppBundle\Form\PersonType', $person);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //***** Here i want to use POST /api/persons/new
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($person);
+            $em->flush();
+
+            return $this->redirectToRoute('person_show', array('id' => $person->getId()));
+        }
+
+        return $this->render('person/new.html.twig', array(
+            'person' => $person,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a person entity.
+     *
+     * @Route("/{id}", name="person_show")
+     * @Method("GET")
+     */
+    public function showAction(Person $person)
+    {
+        $deleteForm = $this->createDeleteForm($person);
+
+        //***** Here i want to use GET /api/persons/{id}
+
+        return $this->render('person/show.html.twig', array(
+            'person' => $person,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing person entity.
+     *
+     * @Route("/{id}/edit", name="person_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Person $person)
+    {
+        $deleteForm = $this->createDeleteForm($person);
+        $editForm = $this->createForm('AppBundle\Form\PersonType', $person);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+
+            //***** Here i want to use PUT /api/persons/{id}
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('person_edit', array('id' => $person->getId()));
+        }
+
+        return $this->render('person/edit.html.twig', array(
+            'person' => $person,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a person entity.
+     *
+     * @Route("/{id}", name="person_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Person $person)
+    {
+        $form = $this->createDeleteForm($person);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //***** Here i want to use DELETE /api/persons/{id}
+
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($person);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('person_index');
+    }
+
+    /**
+     * Creates a form to delete a person entity.
+     *
+     * @param Person $person The person entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Person $person)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('person_delete', array('id' => $person->getId())))
+            ->setMethod('DELETE')
+            ->getForm();
+    }
+}
